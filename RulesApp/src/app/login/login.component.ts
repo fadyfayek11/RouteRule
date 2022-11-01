026f622d33f84c14d22bfb3c06a0acf034e5e1ca
MO_Najeeb <mohamednajeeb300@gmail.com>
2022-11-01 16:22:12 +0200

    Login updated

--- RulesApp/src/app/login/login.component.ts
+++ RulesApp/src/app/login/login.component.ts
@@ -1,31 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router'
+import { Subject } from 'rxjs';
+import * as alertify from 'alertifyjs';
+import { Login } from '../Models/LoginModel';
 import { apiService } from '../shared/apiService.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private api: apiService) { }
+  // public loginValid = true;
+  // public username = '';
+  // public password = '';
 
-  ngOnInit(): void {
-  }
+  // private _destroySub$ = new Subject<void>();
+  // private readonly returnUrl: string;
 
+  constructor(private api: apiService, private builder: FormBuilder, private _router: Router)
+   {}
 
-usernameControl =new FormControl('', [Validators.required]);
-passwordControl = new FormControl('', [Validators.required]);
+   ngOnInit(): void { }
 
-  form: FormGroup = new FormGroup({
-    username:this.usernameControl,
-    password:this.passwordControl
+  usernameControl = new FormControl('', [Validators.required]);
+  passwordControl = new FormControl('', [Validators.required]);
+
+  userForm = this.builder.group({
+    username: this.usernameControl,
+    password: this.passwordControl,
+    // isactive: this.builder.control(true),
   });
 
+  Submit() {
+    if (this.userForm.valid) {
+      let user: Login = {
+        UserName: this.userForm.get('username')?.value!,
+        Password: this.userForm.get('password')?.value!,
+      };
+      console.log(user);
 
-  submit()
-  {
-
+      this.api.Login(user).subscribe(
+        (response) => {
+          alertify.success('Logging in succesfully');
+        },
+        (error) => {
+          alertify.warning('logging failed');
+        }
+      );
+    }
   }
 }
