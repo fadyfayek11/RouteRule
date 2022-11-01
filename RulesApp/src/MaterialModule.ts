9f58c19e558c4a881f43cf564b2158e065526d16
MO_Najeeb <mohamednajeeb300@gmail.com>
2022-11-01 11:19:55 +0200

    Login form updated

--- RulesApp/src/MaterialModule.ts
+++ RulesApp/src/MaterialModule.ts
@@ -8,7 +8,9 @@ import { MatDialogModule } from '@angular/material/dialog'
 import { MatButtonModule } from '@angular/material/button'
 import { MatCheckboxModule } from '@angular/material/checkbox'
 import { MatCardModule } from '@angular/material/card'
-import { MatSelectModule } from '@angular/material/select'
+import { MatSelectModule } from '@angular/material/select';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 
 
@@ -23,7 +25,9 @@ import { MatSelectModule } from '@angular/material/select'
         MatButtonModule,
         MatCheckboxModule,
         MatCardModule,
-        MatSelectModule
+        MatSelectModule,
+        MatToolbarModule
+
     ]
 
 })
